cada14b84e13f4c23fcd4b38fc90de84b48d9325
Quinton Fults <quinton.fults.work@gmail.com>
2020-08-17 11:49:05 -0700

    code

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  console.log(req.url.split('/'))
 
+  fs.readFile('index.html', 'utf8', (err, html) => {
+    if (err) { console.log(err) }
+    res.end(html)
+  })
+
+  // res.end('Hello World!')
+
+  // let [, op, a, b] = req.url.split('/')
+
+  // a = parseInt(a)
+  // b = parseInt(b)
+
+  // switch (op) {
+  //   case 'add':
+  //     res.end(`${a + b}`)
+  //     break
+  //   case 'subtract':
+  //     res.end(`${a - b}`)
+  //     break
+  //   case 'multiply':
+  //     res.end(`${a * b}`)
+  //     break
+  //   case 'divide':
+  //     res.end(`${a / b}`)
+  //     break
+  // }
 })
 
 server.listen(3000)
